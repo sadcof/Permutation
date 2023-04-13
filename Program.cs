bb0d908704f21da48decb31a620a5f4e4f539d32
sadcof <sadsidecoff@gmail.com>
2023-04-13 13:38:58 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Айеро";
+            string surname = "Фрэнк";
+            Console.WriteLine($"Имя {name}, фамилия {surname}");
+
+            string change = name;
+            name = surname;
+            surname = change;
+
+            Console.WriteLine($"Имя {name}, фамилия {surname}");
+            Console.ReadKey(); 
+        }
+    }
+}
